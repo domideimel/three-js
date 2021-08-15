b4e41ba7632a74569c6fd47968281d3d7ced7f00
Dominik <info@dominikdeimel.com>
2021-08-15 19:17:01 +0200

    chore: added image minification at build

---
+++ vite.config.ts
@@ -0,0 +1,30 @@
+import viteImagemin from 'vite-plugin-imagemin';
+
+export default () => {
+  return {
+    plugins: [viteImagemin({
+      gifsicle: {
+        optimizationLevel: 7,
+        interlaced: false
+      },
+      optipng: {
+        optimizationLevel: 7
+      },
+      mozjpeg: {
+        quality: 20
+      },
+      pngquant: {
+        quality: [0.8, 0.9],
+        speed: 4
+      },
+      svgo: {
+        plugins: [{
+          name: 'removeViewBox'
+        }, {
+          name: 'removeEmptyAttrs',
+          active: false
+        }]
+      }
+    })]
+  };
+};
